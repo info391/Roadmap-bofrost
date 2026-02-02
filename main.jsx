622f4ce7c07270b4c1c3c320e16f4b3ef894f1e3
info391 <info@nobilis-house.ch>
2026-02-02 02:07:52 +0100

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 {\rtf1\ansi\ansicpg1252\cocoartf2822
 \cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
 {\colortbl;\red255\green255\blue255;\red183\green111\blue247;\red23\green24\blue24;\red202\green202\blue202;
-\red54\green192\blue160;\red113\green192\blue131;\red109\green115\blue120;\red212\green212\blue212;}
+\red54\green192\blue160;\red113\green192\blue131;\red212\green212\blue212;}
 {\*\expandedcolortbl;;\cssrgb\c77255\c54118\c97647;\cssrgb\c11765\c12157\c12549;\cssrgb\c83137\c83137\c83137;
-\cssrgb\c23922\c78824\c69020;\cssrgb\c50588\c78824\c58431;\cssrgb\c50196\c52549\c54510;\cssrgb\c86275\c86275\c86275;}
+\cssrgb\c23922\c78824\c69020;\cssrgb\c50588\c78824\c58431;\cssrgb\c86275\c86275\c86275;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \deftab720
 \pard\pardeftab720\partightenfactor0
@@ -14,13 +14,11 @@
 \cf2 \cb3 \strokec2 import\cf4 \strokec4  \cf5 \strokec5 App\cf4 \strokec4  \cf2 \strokec2 from\cf4 \strokec4  \cf6 \strokec6 './App.jsx'\cf4 \cb1 \strokec4 \
 \
 \pard\pardeftab720\partightenfactor0
-\cf7 \cb3 \strokec7 // Montage de l'application dans la balise root de index.html\cf4 \cb1 \strokec4 \
+\cf5 \cb3 \strokec5 ReactDOM\cf7 \strokec7 .\cf4 \strokec4 createRoot\cf7 \strokec7 (\cf4 \strokec4 document\cf7 \strokec7 .\cf4 \strokec4 getElementById\cf7 \strokec7 (\cf6 \strokec6 'root'\cf7 \strokec7 )).\cf4 \strokec4 render\cf7 \strokec7 (\cf4 \cb1 \strokec4 \
 \pard\pardeftab720\partightenfactor0
-\cf5 \cb3 \strokec5 ReactDOM\cf8 \strokec8 .\cf4 \strokec4 createRoot\cf8 \strokec8 (\cf4 \strokec4 document\cf8 \strokec8 .\cf4 \strokec4 getElementById\cf8 \strokec8 (\cf6 \strokec6 'root'\cf8 \strokec8 )).\cf4 \strokec4 render\cf8 \strokec8 (\cf4 \cb1 \strokec4 \
+\cf4 \cb3   \cf7 \strokec7 <\cf5 \strokec5 React\cf7 \strokec7 .\cf5 \strokec5 StrictMode\cf7 \strokec7 >\cf4 \cb1 \strokec4 \
+\cb3     \cf7 \strokec7 <\cf5 \strokec5 App\cf4 \strokec4  />\cb1 \
+\cb3   \cf7 \strokec7 </\cf5 \strokec5 React\cf7 \strokec7 .\cf5 \strokec5 StrictMode\cf7 \strokec7 >,\cf4 \cb1 \strokec4 \
 \pard\pardeftab720\partightenfactor0
-\cf4 \cb3   \cf8 \strokec8 <\cf5 \strokec5 React\cf8 \strokec8 .\cf5 \strokec5 StrictMode\cf8 \strokec8 >\cf4 \cb1 \strokec4 \
-\cb3     \cf8 \strokec8 <\cf5 \strokec5 App\cf4 \strokec4  />\cb1 \
-\cb3   \cf8 \strokec8 </\cf5 \strokec5 React\cf8 \strokec8 .\cf5 \strokec5 StrictMode\cf8 \strokec8 >,\cf4 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf8 \cb3 \strokec8 )\cf4 \cb1 \strokec4 \
+\cf7 \cb3 \strokec7 )\cf4 \cb1 \strokec4 \
 }
